165ec253e698cbb6dae81db76cd89c6ddde8acd2
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-01-03 19:34:39 +0300

    Count-Occurences Solved

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,15 @@
-function countOccurrences() {}
+function countOccurrences(string, char) {
+      let count = 0;
+      for(let i=0; i<string.length; i++){
+         if(string[i].toLowerCase() === char.toLowerCase()){
+            count += 1;
+         }
+      }
+      return count;
+   
+   // A second solution. 
+   // return string.split(char).length - 1;
+   
+}
 
 module.exports = countOccurrences;
