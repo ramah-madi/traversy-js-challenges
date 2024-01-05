d44714a64fe4700305d9664d7134f3b1cabdf29d
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-01-05 22:40:15 +0300

    Palindrome Solved

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,10 @@
-function isPalindrome() {}
+function isPalindrome(string) {
+  let str = string.toLowerCase().replace(/[^a-z0-9]/g, '')
+  let reversd = '';
+  for (let i = str.length -1; 0 <= i; i--) {
+    reversd += str[i];
+  }
+  return reversd === str
+}
 
 module.exports = isPalindrome;
