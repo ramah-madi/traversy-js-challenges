0bf7ff7261a205d0b3a31016610cbae33a2b6462
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-01-05 22:41:17 +0300

    Count-Vowels Solved

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,14 @@
-function countVowels() {}
+function countVowels(string) {
+    const vowels = ['a','i', 'u', 'e', 'o'];
+    let count = 0;
+    for (let v = 0; v < vowels.length; v++) {
+       for (let i = 0; i < string.length; i++) {
+            if(string[i].toLowerCase() === vowels[v]){
+                 count+=1;
+           };
+       }  
+    };
+    return count;
+}
 
 module.exports = countVowels;
