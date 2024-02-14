5b40f9d136156537daf166e5c2edb81c4470599a
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-02-14 23:18:49 +0300

    find max number done

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,14 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+     let max = arr[0]
+     for(let i=0; i< arr.length; i++){
+         if(arr[i] > max){
+            max = arr[i]
+         }
+     }
+     return max
+    
+    // A second solution.
+    // return Math.max(arr);
+}
 
 module.exports = findMaxNumber;
