e0a936e840eef0d357d43b25918c47130b310105
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-02-14 23:20:16 +0300

    remove duplicates done

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    
+    const uniqueArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if(!uniqueArr.includes(arr[i])){
+         uniqueArr.push(arr[i]);
+      }  
+    }
+
+    return uniqueArr;
+    
+    // return  Array.from(new Set(arr));
+};
 
 module.exports = removeDuplicates;
