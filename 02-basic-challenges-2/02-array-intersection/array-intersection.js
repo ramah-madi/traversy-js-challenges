3e128000baf24d493921c2ec6af7a1edd957b25c
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-04-18 18:08:45 +0300

    array intersection solved

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,26 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  //Solution 1
+  // const intersectedArr = []
+  // for(let i = 0; i < arr1.length; i++) {
+
+  //     if (arr2.includes(arr1[i]) && !intersectedArr.includes(arr1[i])) {
+  //       intersectedArr.push(arr1[i])
+  //     }; 
+  // };
+ 
+  // return intersectedArr;
+
+  // Solution 2
+     const set1 = new Set(arr1);
+     const intersectedArr = [];
+
+     for(let num of arr2) {
+      if(set1.has(num)){
+        intersectedArr.push(num);
+      };
+     };
+     
+     return intersectedArr;
+}
 
 module.exports = arrayIntersection;
