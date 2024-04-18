980554f4b86daed7d5933c81d859009bb8fcf489
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-04-18 19:38:43 +0300

    display likes solved

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,22 @@
-function displayLikes() {}
+function displayLikes(arr) {
+  let likeString = ``;
+  if(arr.length > 3) {
+    likeString = `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+
+  } else if (arr.length === 3) {
+    likeString = `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+
+  } else if (arr.length === 2) {
+    likeString = `${arr[0]} and ${arr[1]} like this`;
+
+  } else if (arr.length === 1) {
+    likeString = `${arr[0]} likes this`;
+
+  } else {
+    likeString = `no one likes this`;
+  }
+
+  return likeString
+}
 
 module.exports = displayLikes;
