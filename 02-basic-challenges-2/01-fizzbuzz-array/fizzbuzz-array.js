195071429bd16c4a82f9aa49d9c4e675ee1d9996
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-04-18 17:29:37 +0300

    fizzbuzz solved

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(number) {
+  let arr = [];
+  for (let i=1; i <= number; i++) {
+    if (i%3===0 && i%5 ===0) {
+        arr.push("FizzBuzz");
+    } else if (i%3===0) {
+        arr.push("Fizz");
+    } else if (i%5===0) {
+        arr.push("Buzz");
+    } else {
+        arr.push(i)
+    };
+  };
+  return arr
+};
 
 module.exports = fizzBuzzArray;
