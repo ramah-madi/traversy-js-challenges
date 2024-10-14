201f31e9a26e01f1d288ddbda5f9daaa34d3a65c
ramah-madi <ramahmadi2001@gmail.com>
2024-10-14 16:22:51 +0300

    find missing number solved

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,17 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  const uniqueArr = Array.from(new Set(arr));
+  
+  if(uniqueArr.length === 0) return 1;
+
+  const n = uniqueArr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
+  for(let i = 0; i < uniqueArr.length; i++) {
+      actualSum += uniqueArr[i];
+  };
+  
+  return expectedSum - actualSum;
+};
 
 module.exports = findMissingNumber;
