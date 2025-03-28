322cfc1dd068c6654055d04596ba018a97e87b3f
ramah-madi <ramahmadi2001@gmail.com>
2025-03-28 17:00:25 +0300

    format phone number solved

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,9 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  const areaCode = arr.slice(0, 3).join('');
+  const prefix = arr.slice(3, 6).join('');
+  const lineNumbers = arr.slice(6).join('');
+
+  return `(${areaCode}) ${prefix}-${lineNumbers}`;
+}
 
 module.exports = formatPhoneNumber;
