2046dc4916b973a0b221a7dd6cadeb942984e67a
ramah-madi <ramahmadi2001@gmail.com>
2025-05-03 22:04:06 +0300

    validate email solved

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,12 @@
-function validateEmail() {}
+function validateEmail(email) {
+  const arr = email.split('');
+  const charSet = new Set(arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (charSet.has('@') && charSet.has('.')) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = validateEmail;
