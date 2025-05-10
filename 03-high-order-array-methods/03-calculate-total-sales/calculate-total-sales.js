1efa0c846f76d4cb0aef494cc6d92f6d0d14b8fe
ramah-madi <ramahmadi2001@gmail.com>
2025-05-10 18:21:18 +0300

    calculate total sales solved

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,9 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(arr, tax) {
+  let totalSales = 0;
+  arr.forEach((item) => {
+    totalSales += item.price * item.quantity;
+  });
+  return totalSales + (totalSales * tax) / 100;
+}
 
 module.exports = calculateTotalSalesWithTax;
