974fb343d90bd760ca38fed26f12e395d8b072f5
ramah-madi <ramahmadi2001@gmail.com>
2025-05-11 16:55:35 +0300

    highest scoring word solved

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,21 @@
-function highestScoringWord() {}
+function highestScoringWord(sentence) {
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    const words = sentence.toLowerCase().split(' ');
+    const letters = words.map(word => {
+        return word.split('');
+    });
+
+    const scores = letters.map(word => {
+         return word.reduce((acc, letter) => {
+            return acc + (alphabet.indexOf(letter) + 1);
+        }, 0);
+    });
+
+    const maxScore = Math.max(...scores);
+    const maxIndex = scores.indexOf(maxScore);
+    return words[maxIndex];
+}
 
 module.exports = highestScoringWord;
