00b63f223150681bba9a4ab82c9be17a5bee1890
ramah-madi <ramahmadi2001@gmail.com>
2025-05-11 15:27:06 +0300

    update validate email solution

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,11 +1,9 @@
 function validateEmail(email) {
   const arr = email.split('');
   const charSet = new Set(arr);
-  for (let i = 0; i < arr.length; i++) {
     if (charSet.has('@') && charSet.has('.')) {
       return true;
     }
-  }
   return false;
 }
 
