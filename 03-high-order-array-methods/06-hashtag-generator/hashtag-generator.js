7877652d018939ad7c868f113258c7206845511c
ramah-madi <ramahmadi2001@gmail.com>
2025-05-12 15:02:43 +0300

    hashtag generator solved

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,13 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+    if (str.length === 0 || str.length > 140) {
+        return false;
+    };
+
+    const hashedString = str
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join('')
+    return `#${hashedString}`;
+}
 
 module.exports = generateHashtag;
